3473db8054e3630284635b667fca731a20bd8f12
Ragnov <mkins52@gmail.com>
2020-12-01 11:04:45 +0800

    added the input of a user

--- PythonApplication5/PythonApplication5/PythonApplication5.py
+++ PythonApplication5/PythonApplication5/PythonApplication5.py
@@ -1,233 +1,341 @@
+
 from math import floor
 
+print("Algorithm Sorter")
+numbers = []
+while True:
+    while True:
 
-numbers = [8,5,60,25,45,35,12,1]
+        choice = ''
 
+        while True:
 
-#selection_sort
-def selection_sort(numbers):
-    indexing_length = range(0, len(numbers)-1)
+            print(numbers)
+            print("add number: ")
+            add = int(input())
+            numbers.append(add)
+            print(numbers)
 
-    for i in indexing_length:
-        min_value = i
-
-        for j in range(i+1, len(numbers)):
-            if numbers[j] < numbers[min_value]: 
-                min_value = j
-
-
-        if min_value != i:
-            numbers[min_value], numbers[i] = numbers[i], numbers[min_value]
-
-    return numbers
-print("selection sort: " )
-print(selection_sort(numbers))
-
-#bubble_sort
-def bubble_sort(numbers):
-    for i in range(len(numbers)-1,0,-1):
-        for j in range(i):
-            if numbers[j]>numbers[j+1]:
-                temporary = numbers[j]
-                numbers[j] = numbers[j+1]
-                numbers[j+1]=temporary
-
-
-
-bubble_sort(numbers)
-print("bubble sort: " )
-print ( numbers)
-
-#insertion_sort
-def insertion_sort(numbers):
-    indexing_length = range(1, len(numbers))
-    for i in indexing_length:
-        value_to_sort = numbers[i]
-
-        while numbers[i-1] > value_to_sort and i > 0:
-            numbers[i], numbers[i-1] = numbers[i-1], numbers[i]
-            i = i -1
-
-    return numbers
-
-print("insertion sort: " )
-print(insertion_sort(numbers))
-
-#quick_sort
-def quick_sort(numbers):
-    length = len(numbers)
-    if length <= 1:
-        return numbers
-    else:
-        pivot = numbers.pop()
-
-    items_greater = []
-    items_lower = []
-
-    for item in numbers:
-        if item > pivot:
-            items_greater.append(item)
-
-        else:
-            items_lower.append(item)
-
-    return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
-print("quick sort: " )
-print(quick_sort(numbers))
-
-
-
-#shell_sort
-def shellSort(numbers):
-    n = len(numbers)
-    # Gap sequence
-    gap = floor(n / 2)
-    while gap > 0:
-        for i in range(gap, n):
-            temp = numbers[i]
-            j = i
-            # Compare elements at equal gap.
-            while j >= gap and temp < numbers[j - gap]:
-                numbers[j] = numbers[j - gap]
-                j -= gap
-        numbers[j] = temp
-        gap = floor(gap / 2)
-
-
-
-shellSort(numbers)
-print("shell sort: " )
-print(numbers)
-
-
-
-
-#merge_sort
-def mergeSort(numbers):
-    if len(numbers) > 1:
-        mid = len(numbers) // 2
-        left = numbers[:mid]
-        right = numbers[mid:]
-
-  
-        mergeSort(left)
-        mergeSort(right)
-
-        
-        i = 0
-        j = 0
-        
-      
-        k = 0
-        
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-             
-              numbers[k] = left[i]
-              
-              i += 1
+            if input('do you wish to add more numbers? (y or n) ') == 'n':
+                break
             else:
-                numbers[k] = right[j]
-                j += 1
-            
-            k += 1
+                print("I didn't understand that")
+
+        print(numbers)
+        if input('Confirm numbers? (y or n) ') == 'y':
+            break
+
+
+    print('Select sorting algorithm:\n[1]bubble sort\n[2]insertion sort\n[3]shell sort\n[4]merge sort\n[5]bucket sort\n[6]radix sort\n[7]selection sort\n[8]quick sort')
+    algo = int(input())
+
+    if algo == 1:
+        # bubble_sort
+
+        def bubble_sort(numbers):
+
+            for i in range(len(numbers) - 1, 0, -1):
+
+                for j in range(i):
+
+                    if numbers[j] > numbers[j + 1]:
+                        temporary = numbers[j]
+
+                        numbers[j] = numbers[j + 1]
+
+                        numbers[j + 1] = temporary
+
+
+        bubble_sort(numbers)
+
+        print("bubble sort: ")
+
+        print(numbers)
+
+    elif algo == 2:
+        # insertion_sort
+
+        def insertion_sort(numbers):
+
+            indexing_length = range(1, len(numbers))
+
+            for i in indexing_length:
+
+                value_to_sort = numbers[i]
+
+                while numbers[i - 1] > value_to_sort and i > 0:
+                    numbers[i], numbers[i - 1] = numbers[i - 1], numbers[i]
+
+                    i = i - 1
+
+            return numbers
+
+
+        print("insertion sort: ")
+
+        print(insertion_sort(numbers))
+    elif algo == 3:
+        # shell_sort
+
+        def shellSort(numbers):
+
+            n = len(numbers)
+
+            # Gap sequence
+
+            gap = floor(n / 2)
+
+            while gap > 0:
+
+                for i in range(gap, n):
+
+                    temp = numbers[i]
+
+                    j = i
+
+                    # Compare elements at equal gap.
+
+                    while j >= gap and temp < numbers[j - gap]:
+                        numbers[j] = numbers[j - gap]
+
+                        j -= gap
+
+                numbers[j] = temp
+
+                gap = floor(gap / 2)
+
+
+        shellSort(numbers)
+
+        print("shell sort: ")
+
+        print(numbers)
+
+    elif algo == 4:
+        # merge_sort
+
+        def mergeSort(numbers):
+
+            if len(numbers) > 1:
+
+                mid = len(numbers) // 2
+
+                left = numbers[:mid]
+
+                right = numbers[mid:]
+
+                mergeSort(left)
+
+                mergeSort(right)
+
+                i = 0
+
+                j = 0
+
+                k = 0
+
+                while i < len(left) and j < len(right):
+
+                    if left[i] < right[j]:
+
+                        numbers[k] = left[i]
+
+                        i += 1
+
+                    else:
+
+                        numbers[k] = right[j]
+
+                        j += 1
+
+                    k += 1
+
+                while i < len(left):
+                    numbers[k] = left[i]
+
+                    i += 1
+
+                    k += 1
+
+                while j < len(right):
+                    numbers[k] = right[j]
+
+                    j += 1
+
+                    k += 1
+
+        mergeSort(numbers)
+
+        print("merge sort: ")
+
+        print(numbers)
+    elif algo == 5:
+        # bucket_sort
+
+        def insertion(inpvalue):
+            for i in range(1, len(inpvalue)):
+                temp = inpvalue[i]
+                j = i - 1
+                while (j >= 0 and temp < inpvalue[j]):
+                    inpvalue[j + 1] = inpvalue[j]
+                    j = j - 1
+                inpvalue[j + 1] = temp
+
+        def bucket_sort(inpvalue):
+            largest = max(inpvalue)
+            length = len(inpvalue)
+            size = largest/length
+ 
+            buckets = [[] for _ in range(length)]
+            for i in range(length):
+                j = int(inpvalue[i]/size)
+        
+                if j != length:
+                    buckets[j].append(inpvalue[i])
+                else:
+                    buckets[length - 1].append(inpvalue[i])
+ 
+            for i in range(length):
+                insertion(buckets[i])
+ 
+            res = []
+    
+            for i in range(length):
+                res = res + buckets[i]
+ 
+            return res
+ 
+ 
+
+        inpvalue = numbers
+
+        sorted_list = bucket_sort(inpvalue)
+        print("bucket sort: ", end='')
+        print(sorted_list)
+
+
 
         
-        while i < len(left):
-            numbers[k] = left[i]
-            i += 1
-            k += 1
 
-        while j < len(right):
-            numbers[k]=right[j]
-            j += 1
-            k += 1
+       
 
+       
+    elif algo == 6:
+        # radix_sort
 
-mergeSort(numbers)
-print("merge sort: " )
-print(numbers)
+        def countingSort(array, place):
 
-#bucket_sort
-def insertionSort(b): 
-    for i in range(1, len(b)): 
-        up = b[i] 
-        j = i - 1
-        while j >= 0 and b[j] > up:  
-            b[j + 1] = b[j] 
-            j -= 1
-        b[j + 1] = up      
-    return b      
-              
-def bucketSort(x): 
-    arr = [] 
-    slot_num = 10 # 10 means 10 slots, each 
-                  # slot's size is 0.1 
-    for i in range(slot_num): 
-        arr.append([]) 
-          
-    
-    for j in x: 
-        index_b = int(slot_num * j)  
-        arr[index_b].append(j) 
-      
-    # Sort individual buckets  
-    for i in range(slot_num): 
-        arr[i] = insertionSort(arr[i]) 
-          
-    # concatenate the result 
-    k = 0
-    for i in range(slot_num): 
-        for j in range(len(arr[i])): 
-            x[k] = arr[i][j] 
-            k += 1
-    return x 
+            size = len(array)
 
-x = [0.897, 0.565, 0.656, 
-     0.1234, 0.665, 0.3434]  
-print("bucket sort: " )
-print(bucketSort( x)) 
+            output = [0] * size
 
-#radix_sort
-def countingSort(array, place):
-    size = len(array)
-    output = [0] * size
-    count = [0] * 10
+            count = [0] * 10
 
-    
-    for i in range(0, size):
-        index = array[i] // place
-        count[index % 10] += 1
+            for i in range(0, size):
+                index = array[i] // place
 
-   
-    for i in range(1, 10):
-        count[i] += count[i - 1]
+                count[index % 10] += 1
 
-    
-    i = size - 1
-    while i >= 0:
-        index = array[i] // place
-        output[count[index % 10] - 1] = array[i]
-        count[index % 10] -= 1
-        i -= 1
+            for i in range(1, 10):
+                count[i] += count[i - 1]
 
-    for i in range(0, size):
-        array[i] = output[i]
+            i = size - 1
+
+            while i >= 0:
+                index = array[i] // place
+
+                output[count[index % 10] - 1] = array[i]
+
+                count[index % 10] -= 1
+
+                i -= 1
+
+            for i in range(0, size):
+                array[i] = output[i]
 
 
+        def radixSort(array):
 
-def radixSort(array):
-   
-    max_element = max(array)
+            max_element = max(array)
 
-    
-    place = 1
-    while max_element // place > 0:
-        countingSort(array, place)
-        place *= 10
+            place = 1
+
+            while max_element // place > 0:
+                countingSort(array, place)
+
+                place *= 10
 
 
-data = numbers
-radixSort(data)
-print("radix sort: ")
-print(data)
+        data = numbers
+
+        radixSort(data)
+
+        print("radix sort: ")
+
+        print(data)
+    elif algo == 7:
+        # selection_sort
+
+        def selection_sort(numbers):
+
+            indexing_length = range(0, len(numbers) - 1)
+
+            for i in indexing_length:
+
+                min_value = i
+
+                for j in range(i + 1, len(numbers)):
+
+                    if numbers[j] < numbers[min_value]:
+                        min_value = j
+
+                if min_value != i:
+                    numbers[min_value], numbers[i] = numbers[i], numbers[min_value]
+
+            return numbers
+
+
+        print("selection sort: ")
+
+        print(selection_sort(numbers))
+    elif algo == 8:
+        # quick_sort
+
+        def quick_sort(numbers):
+
+            length = len(numbers)
+
+            if length <= 1:
+
+                return numbers
+
+            else:
+
+                pivot = numbers.pop()
+
+            items_greater = []
+
+            items_lower = []
+
+            for item in numbers:
+
+                if item > pivot:
+
+                    items_greater.append(item)
+
+
+
+                else:
+
+                    items_lower.append(item)
+
+            return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
+
+
+        print("quick sort: ")
+
+        print(quick_sort(numbers))
+
+    if input('Continue? (y or n)') != 'y':
+        break
+
